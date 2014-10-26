069a7270c0b7c358f5dcf656bf0826aa2a4d6ae3
Joachim Metz <joachim.metz@gmail.com>
2014-10-26 08:06:00 +0100

    Changes for deployment.

--- libnk2/libnk2_definitions.h
+++ libnk2/libnk2_definitions.h
@@ -37,11 +37,11 @@
  * for local use of libnk2
  */
 #else
-#define LIBNK2_VERSION					20141024
+#define LIBNK2_VERSION					20141026
 
 /* The version string
  */
-#define LIBNK2_VERSION_STRING				"20141024"
+#define LIBNK2_VERSION_STRING				"20141026"
 
 /* The access flags definitions
  * bit 1        set to 1 for read access
