37f2f3b384f32cd30fb523217386deabb21c582d
Joachim Metz <joachim.metz@gmail.com>
2015-09-27 13:02:51 +0200

    Changes of version: 20141001

--- libnk2/libnk2_definitions.h
+++ libnk2/libnk2_definitions.h
@@ -37,11 +37,11 @@
  * for local use of libnk2
  */
 #else
-#define LIBNK2_VERSION					20140723
+#define LIBNK2_VERSION					20141001
 
 /* The version string
  */
-#define LIBNK2_VERSION_STRING				"20140723"
+#define LIBNK2_VERSION_STRING				"20141001"
 
 /* The access flags definitions
  * bit 1        set to 1 for read access
