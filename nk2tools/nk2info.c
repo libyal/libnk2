f8b7e6dd8a3e92a2d87b6946dc924f72339967f5
Joachim Metz <joachim.metz@gmail.com>
2017-01-01 11:10:04 +0100

    Worked on tests and applied updates

--- nk2tools/nk2info.c
+++ nk2tools/nk2info.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from a Nickfile (NK2)
  *
- * Copyright (C) 2009-2016, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2017, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- tests/pynk2_test_file.py
+++ tests/pynk2_test_file.py
@@ -2,7 +2,7 @@
 #
 # Python-bindings file type test script
 #
-# Copyright (C) 2009-2016, Joachim Metz <joachim.metz@gmail.com>
+# Copyright (C) 2009-2017, Joachim Metz <joachim.metz@gmail.com>
 #
 # Refer to AUTHORS for acknowledgements.
 #
@@ -20,6 +20,7 @@
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
+import os
 import sys
 import unittest
 
