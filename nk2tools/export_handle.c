eaa3359dbd2a5e71cd8e01e1316493c5446ba2ed
Joachim Metz <joachim.metz@gmail.com>
2026-06-23 09:14:27 +0200

    Worked on tests and changes for CI tests

--- nk2tools/export_handle.c
+++ nk2tools/export_handle.c
@@ -684,7 +684,7 @@ int export_handle_open(
 }
 
 /* Closes the export handle
- * Returns the 0 if succesful or -1 on error
+ * Returns the 0 if successful or -1 on error
  */
 int export_handle_close(
      export_handle_t *export_handle,
@@ -1611,7 +1611,7 @@ on_error:
 }
 
 /* Exports the items
- * Returns the 1 if succesful, 0 if no items are available or -1 on error
+ * Returns the 1 if successful, 0 if no items are available or -1 on error
  */
 int export_handle_export_items(
      export_handle_t *export_handle,
@@ -1775,7 +1775,7 @@ int export_handle_export_items(
 }
 
 /* Exports the items from the file
- * Returns the 1 if succesful, 0 if no items are available or -1 on error
+ * Returns the 1 if successful, 0 if no items are available or -1 on error
  */
 int export_handle_export_file(
      export_handle_t *export_handle,
